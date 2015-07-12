c368ba2168989bc4648f1df7230675e1d38128ad
Bruno Alves <BrunoCau@MacBook-Pro-de-Bruno.local>
2015-07-12 15:19:28 -0300

    foi criado uma dependencia para o projeto pipeline-nodejs-component

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+
+/**
+* Esse projeto e um estudo de engenharia de software 
+* @property pipeline
+* @type {WebApi}
+* @default "WebApi que retorna numero da versão e o que estra escrito na biblioteca"
+*/
+
 var 
     http = require('http'),
     fs = require('fs'),
+    hello = require('pipeline-nodejs-component'),
     packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
-
 ;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   console.log(process.env.NODE_ENV);
-  res.end(JSON.stringify({ version: packageJson.version }));
+  res.end(JSON.stringify({ version: packageJson.version , msg:hello}));
 
 }).listen(1337);
 console.log('servidor iniciado http://localhost:1337');
