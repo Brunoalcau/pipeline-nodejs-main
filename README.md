a985f3aef2a37b995a4706b43eb80b1891b6f627
Bruno Alves <BrunoCau@MacBook-Pro-de-Bruno.local>
2015-07-16 22:20:23 -0300

    teste commit codeship

--- README.md
+++ README.md
@@ -1,14 +1,2 @@
-Project Example
-
-Just a simple nodejs app, with a module dependencie
-
-The goal is to learn diferent services that can be used to do Continuous Delivery
- - http://continuousdelivery.com/
- - http://www.thoughtworks.com/continuous-delivery
-
-Every commit should generate a new version of the app delivered
-
-There are so many tools to do the job, let's just explore this tools
-
-[Stacks](https://github.com/rsvalerio/pipeline-nodejs-main/wiki/Stacks)
-
+# Nodeship
+Projeto em node.js para teste de integração contínua com o Codeship
--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-
-/**
-* Esse projeto e um estudo de engenharia de software 
-* @property pipeline
-* @type {WebApi}
-* @default "WebApi que retorna numero da versão e o que estra escrito na biblioteca"
-*/
-
 var 
     http = require('http'),
     fs = require('fs'),
-    hello = require('pipeline-nodejs-component'),
     packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+
 ;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  console.log(process.env.NODE_ENV);
-  res.end(JSON.stringify({ version: packageJson.version , msg:hello}));
 
-}).listen(1337);
-console.log('servidor iniciado http://localhost:1337');
+  res.end(JSON.stringify({ version: packageJson.version }));
+
+}).listen(process.env.PORT || 5000);
+console.log('servidor iniciado http://localhost:5000');
